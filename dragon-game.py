9b6dd136e1c8d2d7f4ecddda6d0283593ecaac66
salehhoushangi <46454680+salehhoushangi@users.noreply.github.com>
2019-01-12 23:21:04 +0330

    commite  -m "secondd Edition "

--- dragon-game.py
+++ dragon-game.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 import random
 import os
 dimension_value = int(input("inter your dimension\n"))
@@ -8,28 +9,25 @@ def show_location(i,row):
     y=row
     x=i/2
 
-while True:
-
-    d=random.randint(0,dimension_value-1)
-    b = random.randint(1,dimension_value*2)
-    if b%2 != 0 :
-        break
-    #return(b,d)
 def scape_door():
+    global scap_y,scap_x
     while True:
         scap_y=random.randint(0,dimension_value-1)
         scap_x= int(random.randint(1,dimension_value*2)/2)
         if scap_x%2 != 0 :
             break
     return(scap_x,scap_y)
-def dragon_locate():
-    while True:
 
+
+def dragon_locate():
+    global dragon_x,dragon_y
+    while True:
         dragon_y=random.randint(0,dimension_value-1)
         dragon_x= int(random.randint(1,dimension_value*2)/2)
         if dragon_x%2 != 0 :
             break
-        return(dragon_x,dragon_y)
+    return(dragon_x,dragon_y)
+
 
 def draw_box(i,row):
     user_location=1
@@ -49,60 +47,72 @@ def draw_box(i,row):
                 print("_",end='')
     print("")
     print("you are currently in room({},{})".format(int(x),(y)))
+    
+
 
 def clear_screen():
     os.system('cls' if  os.name == 'nt' else 'clear')
-def main(dragon_locate,scape_door):
-    global b
-    global d
-    (dragon_x,dragon_y) = dragon_locate()
-    (scape_x,scape_y) = scape_door()
+
+while True:
+    d=random.randint(0,dimension_value-1)
+    b = random.randint(1,dimension_value*2)
+    if b%2 != 0 :
+        break
+    #return(b,d) 
+
+draw_box(b,d)
+dragon_x,dragon_y = dragon_locate()
+scape_x,scape_y= scape_door()
+
+while True :
     print('''you can move LEFT , RIGHT , DOWN ,UP
-              Enter 'QUIT' to Exit Game. ''')
+    Enter 'QUIT' to Exit Game. ''')
+    direction=input(" >>").lower()
     condition_lost=bool(b == dragon_x and d == dragon_y)
     condition_win=bool(b == scape_x and d == scap_y)
-    while True :
-            direction=input(" >>").lower()
-            if direction == "right":
-                if b<=(dimension_value*2):
-                    clear_screen()
-                    b=b+1 
-                    show_location(x,y)
-                    draw_box(b,d)
-                else:
-                    draw_box(b,d)
-                    continue
-            if direction == "left":
-                if b>1:
-                    clear_screen()
-                    b=b-1
-                    draw_box(b,d)
-                else:
-                    draw_box(b,d)
-                    continue
-            if direction == "up":
-                if d >=0 :
-                    clear_screen()
-                    d=d-1
-                    draw_box(b,d)
-                else:
-                    draw_box(b,d)
-                    continue
-            if direction == "down":
-                if d>=(dimension_value-1):
-                    clear_screen()
-                    d=d+1
-                    draw_box(b,d)
-                else:
-                    draw_box(b,d)
-                    continue
-            if direction == "quit":
-                print("thanks for gaming")
-                break
-        # if condition_win = True :
-        #          print("congratulation :) you are winner")
-        # else:
-        #      if condition_lost = True :
-        #             print("sorry :( you are GameOver!")
-draw_box(b,d)
-main(scape_door,dragon_locate)
+    if direction == "right":
+        print(b)
+        print(dimension_value)
+        if b<=(dimension_value*2):
+            clear_screen()
+            b=b+2 
+            show_location(x,y)
+            draw_box(b,d)
+        else:
+            draw_box(b,d)
+            continue
+    elif direction == "left":
+        if b>1:
+            clear_screen()
+            b=b-2
+            draw_box(b,d)
+        else:
+            draw_box(b,d)
+            continue
+    elif direction == "up":
+        if d >=0 :
+            clear_screen()
+            d=d-1
+            draw_box(b,d)
+        else:
+            draw_box(b,d)
+            continue
+    elif direction == "down":
+        if d<=(dimension_value-1):
+            clear_screen()
+            d=d+1
+            draw_box(b,d)
+        else:
+            draw_box(b,d)
+            continue
+    elif direction == "quit":
+        print("thanks for gaming")
+        break
+    if (condition_win):
+          print("congratulation :) you are winner")
+          break
+    else:
+        if (condition_lost) :
+            print("sorry :( you are GameOver!")
+            break
+
